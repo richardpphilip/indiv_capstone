e199874ea216eb9d35c1e19682cf0c2d632a2445
richardpphilip <richardpphilip1@gmail.com>
2021-06-03 11:44:17 -0700

    working on sending emails to customers. got the basic framework now I am trying to get the architecture complete.

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('send_email/', views.send_email, name="send_email")
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,43 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+import smtplib
 
 # Create your views here.
+from django.urls import reverse
 
-# TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
+EMAIL_ADDRESS = 'stock.tracker.philip@gmail.com'
+EMAIL_PASSWORD = 'NickMangold74!'
 
 
 def index(request):
     # Get the Customer model from the other app, it can now be used to query the db
     Customer = apps.get_model('customers.Customer')
+
     return render(request, 'employees/index.html')
+
+
+def send_email(request):
+    Account = apps.get_model('accounts.User')
+    users = Account.objects.all()
+    for user in users:
+        if not user.is_employee:
+            print(user.email)
+        else:
+            pass
+    return HttpResponseRedirect(reverse('employees:index'))
+
+    # Customer = apps.get_model('customers.Customer')
+    # customers = Customer.objects.all()
+    # for customer in customers:
+    #     customer.e
+
+    # with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    #     smtp.ehlo()
+    #     smtp.starttls()
+    #     smtp.ehlo()
+    #     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    #     subject = 'Test'
+    #     body = 'Test Test'
+    #     msg = f'Subject:{subject}\n\n{body}'
+    #     smtp.sendmail(EMAIL_ADDRESS,'richardpphilip1@gmail.com', msg)
