f64bf8be078db28ac8f7c8c9fd5a8476e61028d5
richardpphilip <richardpphilip1@gmail.com>
2021-06-03 09:04:33 -0700

    updated the get updated prices button to refresh the stock and position values. tested and it works.

--- trash_collector/positions/views.py
+++ trash_collector/positions/views.py
@@ -5,7 +5,6 @@ import requests
 from django.apps import apps
 from .models import Position
 
-
 # Create your views here.
 headers = {
     'Content-Type': 'application/json',
@@ -27,20 +26,37 @@ def create(request):
         stock_info = price_json.json()
         close_value = stock_info[0]['close']
         stock_close_value = close_value
-        selected_value_int= int(selected_value)
+        selected_value_int = int(selected_value)
         position_value = stock_close_value * selected_value_int
         print(position_value)
-        new_position = Position(selected_ticker=selected_ticker, selected_value=selected_value,stock_close_value=stock_close_value, user_id=user.id, position_value=position_value)
+        new_position = Position(selected_ticker=selected_ticker, selected_value=selected_value,
+                                stock_close_value=stock_close_value, user_id=user.id, position_value=position_value)
         new_position.save()
         return HttpResponseRedirect(reverse('positions:index'))
     else:
         print('error')
         return HttpResponseRedirect(reverse('positions:index'))
 
+
 def update_all(request):
-    print("this works")
-    return render(request, 'positions/update_all.html')
-
-
-
-
+    if request.method == 'POST':
+        print("this works")
+        user = request.user
+        positions = Position.objects.all()
+        for position in positions:
+            if user.id == position.user_id:
+                selected_ticker = position.selected_ticker
+                selected_value = position.selected_value
+                price_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{selected_ticker}/prices',
+                                          headers=headers)
+                stock_info = price_json.json()
+                close_value = stock_info[0]['close']
+                stock_close_value = close_value
+                selected_ticker = position.selected_ticker
+                selected_value_int = int(selected_value)
+                position_value = stock_close_value * selected_value_int
+                position = Position(pk=user.id, selected_value=selected_value,
+                                        stock_close_value=stock_close_value,
+                                        position_value=position_value, selected_ticker=selected_ticker, user_id=user.id)
+                position.save()
+        return HttpResponseRedirect(reverse('customers:index'))
