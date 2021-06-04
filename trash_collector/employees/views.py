d2321c3673d1733ae884b906b1e888892aaa0085
richardpphilip <richardpphilip1@gmail.com>
2021-06-03 18:04:29 -0700

    i got the data to group  by position from the api calls and reset for each user.  need to inject the data into the email and I will be done.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -28,32 +28,48 @@ def send_email(request):
     users = Account.objects.all()
     Position = apps.get_model('positions.Position')
     positions = Position.objects.all()
+
     for user in users:
+        position_list = []
+        stock_news_list = []
+        stock_news_url = []
+        close_value_list = []
+        stock_name_list = []
+
+
         for position in positions:
-            if user.id == position.user_id and user.is_employee == False:
+            if position.user_id == user.id:
+                position_list.append(position)
                 stock_news = requests.get(f'https://api.tiingo.com/tiingo/news?tickers={position}',
                                           headers=headers)
                 stock_news_json = stock_news.json()
-                stock_news_description =stock_news_json[0]['description']
+                stock_news_list.append(stock_news_json[0]['description'])
+                stock_news_url.append(stock_news_json[0]['url'])
                 price_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}/prices',
                                           headers=headers)
                 stock_info = price_json.json()
+                close_value_list.append(stock_info[0]['close'])
                 meta_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}',
                                          headers=headers)
                 stock_meta = meta_json.json()
-                stock_name = stock_meta['name']
-                close_value = stock_info[0]['close']
-                # with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-                #     smtp.ehlo()
-                #     smtp.starttls()
-                #     smtp.ehlo()
-                #     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-                #     subject = f'{stock_name} Stock Update'
-                #     body = f' The closing value for {position} was ${close_value} yesterday.'
-                #     msg = f'Subject:{subject}\n\n{body}'
-                #     smtp.sendmail(EMAIL_ADDRESS, f'{user.email}', msg)
-            else:
-                print(user.first_name)
+                stock_name_list.append(stock_meta['name'])
+        print(user.username)
+        print(stock_name_list)
+        print(position_list)
+        print(stock_news_list)
+        print(close_value_list)
+
+
+
+            # with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            #     smtp.ehlo()
+            #     smtp.starttls()
+            #     smtp.ehlo()
+            #     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            #     subject = f'{stock_name} Stock Update'
+            #     body = f' The closing value for {position} was ${close_value} yesterday.'
+            #     msg = f'Subject:{subject}\n\n{body}'
+            #     smtp.sendmail(EMAIL_ADDRESS, f'{user.email}', msg)
     return HttpResponseRedirect(reverse('employees:index'))
 
     # Customer = apps.get_model('customers.Customer')
