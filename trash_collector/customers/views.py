27ee1de5ef36f4a34d60c87fa07aa3a096ebabd4
richardpphilip <richardpphilip1@gmail.com>
2021-06-07 14:01:16 -0700

    removing unneeded code.

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -11,8 +11,6 @@ import datetime
 
 import json
 
-# Create your views here.
-
 
 headers = {
     'Content-Type': 'application/json',
@@ -40,30 +38,7 @@ def stock(request):
 
 
 def apicall(request):
-    ticker = request.GET['ticker']
-    price_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{ticker}/prices',
-                              headers=headers)
-    stock_info = price_json.json()
-
-    meta_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{ticker}',
-                             headers=headers)
-    stock_meta = meta_json.json()
-    stock_name = stock_meta['name']
-    stock_description = stock_meta['description']
-    close_value = stock_info[0]['close']
-    high_value = stock_info[0]['high']
-    low_value = stock_info[0]['low']
-    volume = stock_info[0]['volume']
-
-    return render(request, 'customers/stock.html',
-                  {'stock_info': stock_info, 'ticker': ticker, 'close_value': close_value, 'high_value': high_value,
-                   'low_value': low_value, 'volume': volume, 'stock_name': stock_name,
-                   'stock_description': stock_description})
-
-
-def stock_details(request, position_id):
-    Position = apps.get_model('positions.Position')
-    position = Position.objects.get(pk=position_id)
+    position = request.GET['ticker']
     today = datetime.date.today()
     five_delta = datetime.timedelta(-10)
     early_day = today + five_delta
@@ -80,7 +55,6 @@ def stock_details(request, position_id):
     stock_url_four = stock_news_json[3]['url']
     stock_news_five = stock_news_json[4]['description']
     stock_url_five = stock_news_json[4]['url']
-
 
     price_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}/prices',
                               headers=headers)
@@ -114,7 +88,75 @@ def stock_details(request, position_id):
 
     return render(request, 'customers/stock_details.html',
                   {'position': position, 'historical_data_json': historical_data_json,
-                   'historical_prices': historical_prices, 'historical_dates': historical_dates, 'active_dates': active_dates, 'close_value': close_value, 'high_value': high_value,
+                   'historical_prices': historical_prices, 'historical_dates': historical_dates,
+                   'active_dates': active_dates, 'close_value': close_value, 'high_value': high_value,
                    'low_value': low_value, 'volume': volume, 'stock_name': stock_name,
-                   'stock_description': stock_description, 'stock_news_json': stock_news_json, 'stock_news_one': stock_news_one,'stock_url_one': stock_url_one, 'stock_news_two': stock_news_two, 'stock_url_two': stock_url_two, 'stock_news_three': stock_news_three, 'stock_url_three': stock_url_three, 'stock_news_four': stock_news_four, 'stock_url_four': stock_url_four, 'stock_news_five': stock_news_five, 'stock_url_five': stock_url_five})
+                   'stock_description': stock_description, 'stock_news_json': stock_news_json,
+                   'stock_news_one': stock_news_one, 'stock_url_one': stock_url_one, 'stock_news_two': stock_news_two,
+                   'stock_url_two': stock_url_two, 'stock_news_three': stock_news_three,
+                   'stock_url_three': stock_url_three, 'stock_news_four': stock_news_four,
+                   'stock_url_four': stock_url_four, 'stock_news_five': stock_news_five,
+                   'stock_url_five': stock_url_five})
 
+
+def stock_details(request, position_id):
+    Position = apps.get_model('positions.Position')
+    position = Position.objects.get(pk=position_id)
+    today = datetime.date.today()
+    five_delta = datetime.timedelta(-10)
+    early_day = today + five_delta
+    stock_news = requests.get(f'https://api.tiingo.com/tiingo/news?tickers={position}',
+                              headers=headers)
+    stock_news_json = stock_news.json()
+    stock_news_one = stock_news_json[0]['description']
+    stock_url_one = stock_news_json[0]['url']
+    stock_news_two = stock_news_json[1]['description']
+    stock_url_two = stock_news_json[1]['url']
+    stock_news_three = stock_news_json[2]['description']
+    stock_url_three = stock_news_json[2]['url']
+    stock_news_four = stock_news_json[3]['description']
+    stock_url_four = stock_news_json[3]['url']
+    stock_news_five = stock_news_json[4]['description']
+    stock_url_five = stock_news_json[4]['url']
+
+    price_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}/prices',
+                              headers=headers)
+    stock_info = price_json.json()
+
+    meta_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}',
+                             headers=headers)
+    stock_meta = meta_json.json()
+    stock_name = stock_meta['name']
+    stock_description = stock_meta['description']
+    close_value = stock_info[0]['close']
+    high_value = stock_info[0]['high']
+    low_value = stock_info[0]['low']
+    volume = stock_info[0]['volume']
+    historical_data = requests.get(
+        f'https://api.tiingo.com/tiingo/daily/{position}/prices?startDate={early_day}&endDate={today}&format=json&resampleFreq=daily',
+        headers=headers)
+    historical_data_json = historical_data.json()
+    active_dates = []
+
+    for i in range(len(historical_data_json)):
+        date_time = datetime.datetime.strptime(historical_data_json[i]['date'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        date_time_list = date_time.strftime("%B %d")
+        active_dates.append(date_time_list)
+
+    historical_prices = []
+    historical_dates = []
+    for i in range(len(historical_data_json)):
+        historical_prices.append(historical_data_json[i]['close'])
+        historical_dates.append(historical_data_json[i]['date'])
+
+    return render(request, 'customers/stock_details.html',
+                  {'position': position, 'historical_data_json': historical_data_json,
+                   'historical_prices': historical_prices, 'historical_dates': historical_dates,
+                   'active_dates': active_dates, 'close_value': close_value, 'high_value': high_value,
+                   'low_value': low_value, 'volume': volume, 'stock_name': stock_name,
+                   'stock_description': stock_description, 'stock_news_json': stock_news_json,
+                   'stock_news_one': stock_news_one, 'stock_url_one': stock_url_one, 'stock_news_two': stock_news_two,
+                   'stock_url_two': stock_url_two, 'stock_news_three': stock_news_three,
+                   'stock_url_three': stock_url_three, 'stock_news_four': stock_news_four,
+                   'stock_url_four': stock_url_four, 'stock_news_five': stock_news_five,
+                   'stock_url_five': stock_url_five})
