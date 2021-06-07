9fc99b9281c5e19e655ee2be3e150854d98dace0
richardpphilip <richardpphilip1@gmail.com>
2021-06-07 09:29:16 -0700

    got the emails to send with the aggregated data I want.  need to work on styling and getting the url to end.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 import requests
 from django.apps import apps
 import smtplib
+from email.message import EmailMessage
+import datetime
+
 
 # Create your views here.
 headers = {
@@ -12,8 +15,7 @@ headers = {
 }
 from django.urls import reverse
 
-EMAIL_ADDRESS = 'stock.tracker.philip@gmail.com'
-EMAIL_PASSWORD = 'NickMangold74!'
+
 
 
 def index(request):
@@ -28,14 +30,16 @@ def send_email(request):
     users = Account.objects.all()
     Position = apps.get_model('positions.Position')
     positions = Position.objects.all()
+    today = datetime.date.today()
 
     for user in users:
         position_list = []
         stock_news_list = []
+        stock_email_list_string = ''
+        stock_email_price_string = ''
         stock_news_url = []
         close_value_list = []
         stock_name_list = []
-
 
         for position in positions:
             if position.user_id == user.id:
@@ -43,36 +47,38 @@ def send_email(request):
                 stock_news = requests.get(f'https://api.tiingo.com/tiingo/news?tickers={position}',
                                           headers=headers)
                 stock_news_json = stock_news.json()
-                stock_news_list.append(stock_news_json[0]['description'])
-                stock_news_url.append(stock_news_json[0]['url'])
+
                 price_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}/prices',
                                           headers=headers)
                 stock_info = price_json.json()
-                close_value_list.append(stock_info[0]['close'])
                 meta_json = requests.get(f'https://api.tiingo.com/tiingo/daily/{position}',
                                          headers=headers)
                 stock_meta = meta_json.json()
-                stock_name_list.append(stock_meta['name'])
-        print(user.username)
-        print(stock_name_list)
-        print(position_list)
-        print(stock_news_list)
-        print(close_value_list)
+                stock_email_list_string += "Update for Stock:"
+                stock_email_list_string += stock_meta['name']
+                stock_email_list_string += "-"
+                stock_email_list_string += f"The price of the Stock as of {today} is:"
+                stock_email_price_string = str(stock_info[0]['close'])
+                stock_email_list_string += stock_email_price_string
+                stock_email_list_string += "-"
+                stock_email_list_string += "A snippet of news I think you might find helpful is:"
+                stock_email_list_string += stock_news_json[0]['description']
+                stock_email_list_string += "/"
+                stock_email_list_string += "-"
+                stock_email_list_string += "If you want to read more this is the link:"
+                stock_email_list_string += stock_news_json[0]['url']
+                stock_email_list_string += "----------------------"
+        EMAIL_ADDRESS = 'stock.tracker.philip@gmail.com'
+        EMAIL_PASSWORD = 'NickMangold74!'
+        print(stock_email_list_string)
+        msg = EmailMessage()
 
-
-
-            # with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-            #     smtp.ehlo()
-            #     smtp.starttls()
-            #     smtp.ehlo()
-            #     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            #     subject = f'{stock_name} Stock Update'
-            #     body = f' The closing value for {position} was ${close_value} yesterday.'
-            #     msg = f'Subject:{subject}\n\n{body}'
-            #     smtp.sendmail(EMAIL_ADDRESS, f'{user.email}', msg)
+        msg['Subject'] = f'Stock Update {user.username} for {today}'
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = f'{user.email}'
+        msg.set_content(stock_email_list_string)
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
     return HttpResponseRedirect(reverse('employees:index'))
 
-    # Customer = apps.get_model('customers.Customer')
-    # customers = Customer.objects.all()
-    # for customer in customers:
-    #     customer.e
